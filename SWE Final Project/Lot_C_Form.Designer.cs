812cf0798f9c33327234305757112d0f0a27decc
Yen Le <yhle@usf.edu>
2022-04-20 00:41:44 -0400

    Interface change

--- SWE Final Project/Lot_C_Form.Designer.cs
+++ SWE Final Project/Lot_C_Form.Designer.cs
@@ -41,143 +41,207 @@ namespace SWE_Final_Project
             this.CHK_C1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.Butt_CHK_A = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // CHK_C10
             // 
             this.CHK_C10.AutoSize = true;
-            this.CHK_C10.Location = new System.Drawing.Point(367, 282);
+            this.CHK_C10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C10.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C10.Location = new System.Drawing.Point(396, 460);
+            this.CHK_C10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C10.Name = "CHK_C10";
-            this.CHK_C10.Size = new System.Drawing.Size(63, 17);
+            this.CHK_C10.Size = new System.Drawing.Size(98, 32);
             this.CHK_C10.TabIndex = 20;
             this.CHK_C10.Text = "Lot C10";
-            this.CHK_C10.UseVisualStyleBackColor = true;
+            this.CHK_C10.UseVisualStyleBackColor = false;
             this.CHK_C10.CheckedChanged += new System.EventHandler(this.CHK_C10_CheckedChanged);
             // 
             // CHK_C9
             // 
             this.CHK_C9.AutoSize = true;
-            this.CHK_C9.Location = new System.Drawing.Point(520, 242);
+            this.CHK_C9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C9.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C9.Location = new System.Drawing.Point(396, 404);
+            this.CHK_C9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C9.Name = "CHK_C9";
-            this.CHK_C9.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C9.Size = new System.Drawing.Size(90, 32);
             this.CHK_C9.TabIndex = 19;
             this.CHK_C9.Text = "Lot C9";
-            this.CHK_C9.UseVisualStyleBackColor = true;
+            this.CHK_C9.UseVisualStyleBackColor = false;
             this.CHK_C9.CheckedChanged += new System.EventHandler(this.CHK_C9_CheckedChanged);
             // 
             // CHK_C8
             // 
             this.CHK_C8.AutoSize = true;
-            this.CHK_C8.Location = new System.Drawing.Point(520, 194);
+            this.CHK_C8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C8.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C8.Location = new System.Drawing.Point(396, 350);
+            this.CHK_C8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C8.Name = "CHK_C8";
-            this.CHK_C8.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C8.Size = new System.Drawing.Size(91, 32);
             this.CHK_C8.TabIndex = 18;
             this.CHK_C8.Text = "Lot C8";
-            this.CHK_C8.UseVisualStyleBackColor = true;
+            this.CHK_C8.UseVisualStyleBackColor = false;
             this.CHK_C8.CheckedChanged += new System.EventHandler(this.CHK_C8_CheckedChanged);
             // 
             // CHK_C7
             // 
             this.CHK_C7.AutoSize = true;
-            this.CHK_C7.Location = new System.Drawing.Point(520, 155);
+            this.CHK_C7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C7.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C7.Location = new System.Drawing.Point(396, 294);
+            this.CHK_C7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C7.Name = "CHK_C7";
-            this.CHK_C7.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C7.Size = new System.Drawing.Size(90, 32);
             this.CHK_C7.TabIndex = 17;
             this.CHK_C7.Text = "Lot C7";
-            this.CHK_C7.UseVisualStyleBackColor = true;
+            this.CHK_C7.UseVisualStyleBackColor = false;
             this.CHK_C7.CheckedChanged += new System.EventHandler(this.CHK_C7_CheckedChanged);
             // 
             // CHK_C6
             // 
             this.CHK_C6.AutoSize = true;
-            this.CHK_C6.Location = new System.Drawing.Point(520, 113);
+            this.CHK_C6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C6.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C6.Location = new System.Drawing.Point(396, 238);
+            this.CHK_C6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C6.Name = "CHK_C6";
-            this.CHK_C6.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C6.Size = new System.Drawing.Size(90, 32);
             this.CHK_C6.TabIndex = 16;
             this.CHK_C6.Text = "Lot C6";
-            this.CHK_C6.UseVisualStyleBackColor = true;
+            this.CHK_C6.UseVisualStyleBackColor = false;
             this.CHK_C6.CheckedChanged += new System.EventHandler(this.CHK_C6_CheckedChanged);
             // 
             // CHK_C5
             // 
             this.CHK_C5.AutoSize = true;
-            this.CHK_C5.Location = new System.Drawing.Point(520, 75);
+            this.CHK_C5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C5.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C5.Location = new System.Drawing.Point(227, 460);
+            this.CHK_C5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C5.Name = "CHK_C5";
-            this.CHK_C5.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C5.Size = new System.Drawing.Size(91, 32);
             this.CHK_C5.TabIndex = 15;
             this.CHK_C5.Text = "Lot C5";
-            this.CHK_C5.UseVisualStyleBackColor = true;
+            this.CHK_C5.UseVisualStyleBackColor = false;
             this.CHK_C5.CheckedChanged += new System.EventHandler(this.CHK_C5_CheckedChanged);
             // 
             // CHK_C4
             // 
             this.CHK_C4.AutoSize = true;
-            this.CHK_C4.Location = new System.Drawing.Point(224, 242);
+            this.CHK_C4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C4.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C4.Location = new System.Drawing.Point(227, 404);
+            this.CHK_C4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C4.Name = "CHK_C4";
-            this.CHK_C4.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C4.Size = new System.Drawing.Size(92, 32);
             this.CHK_C4.TabIndex = 14;
             this.CHK_C4.Text = "Lot C4";
-            this.CHK_C4.UseVisualStyleBackColor = true;
+            this.CHK_C4.UseVisualStyleBackColor = false;
             this.CHK_C4.CheckedChanged += new System.EventHandler(this.CHK_C4_CheckedChanged);
             // 
             // CHK_C3
             // 
             this.CHK_C3.AutoSize = true;
-            this.CHK_C3.Location = new System.Drawing.Point(224, 177);
+            this.CHK_C3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C3.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C3.Location = new System.Drawing.Point(228, 350);
+            this.CHK_C3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C3.Name = "CHK_C3";
-            this.CHK_C3.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C3.Size = new System.Drawing.Size(91, 32);
             this.CHK_C3.TabIndex = 13;
             this.CHK_C3.Text = "Lot C3";
-            this.CHK_C3.UseVisualStyleBackColor = true;
+            this.CHK_C3.UseVisualStyleBackColor = false;
             this.CHK_C3.CheckedChanged += new System.EventHandler(this.CHK_C3_CheckedChanged);
             // 
             // CHK_C2
             // 
             this.CHK_C2.AutoSize = true;
-            this.CHK_C2.Location = new System.Drawing.Point(224, 127);
+            this.CHK_C2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C2.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C2.Location = new System.Drawing.Point(228, 294);
+            this.CHK_C2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C2.Name = "CHK_C2";
-            this.CHK_C2.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C2.Size = new System.Drawing.Size(91, 32);
             this.CHK_C2.TabIndex = 12;
             this.CHK_C2.Text = "Lot C2";
-            this.CHK_C2.UseVisualStyleBackColor = true;
+            this.CHK_C2.UseVisualStyleBackColor = false;
             this.CHK_C2.CheckedChanged += new System.EventHandler(this.CHK_C2_CheckedChanged);
             // 
             // CHK_C1
             // 
             this.CHK_C1.AutoSize = true;
-            this.CHK_C1.Location = new System.Drawing.Point(224, 75);
+            this.CHK_C1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CHK_C1.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 13.2F);
+            this.CHK_C1.Location = new System.Drawing.Point(228, 238);
+            this.CHK_C1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CHK_C1.Name = "CHK_C1";
-            this.CHK_C1.Size = new System.Drawing.Size(57, 17);
+            this.CHK_C1.Size = new System.Drawing.Size(87, 32);
             this.CHK_C1.TabIndex = 11;
             this.CHK_C1.Text = "Lot C1";
-            this.CHK_C1.UseVisualStyleBackColor = true;
+            this.CHK_C1.UseVisualStyleBackColor = false;
             this.CHK_C1.CheckedChanged += new System.EventHandler(this.CHK_C1_CheckedChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(520, 323);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 12F);
+            this.button1.Location = new System.Drawing.Point(117, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 66);
+            this.button1.Size = new System.Drawing.Size(102, 49);
             this.button1.TabIndex = 22;
-            this.button1.Text = "Go Back";
+            this.button1.Text = "< Go Back";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Butt_CHK_A
             // 
-            this.Butt_CHK_A.Location = new System.Drawing.Point(130, 323);
+            this.Butt_CHK_A.FlatAppearance.BorderSize = 0;
+            this.Butt_CHK_A.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Butt_CHK_A.Font = new System.Drawing.Font("Bahnschrift SemiLight SemiConde", 11F);
+            this.Butt_CHK_A.Location = new System.Drawing.Point(288, 519);
+            this.Butt_CHK_A.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Butt_CHK_A.Name = "Butt_CHK_A";
-            this.Butt_CHK_A.Size = new System.Drawing.Size(151, 66);
+            this.Butt_CHK_A.Size = new System.Drawing.Size(164, 65);
             this.Butt_CHK_A.TabIndex = 21;
             this.Butt_CHK_A.Text = "Check for Parking";
             this.Butt_CHK_A.UseVisualStyleBackColor = true;
             this.Butt_CHK_A.Click += new System.EventHandler(this.Butt_CHK_A_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 28.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(214, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 59);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Welcome to Lot C!";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(117, 186);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(499, 428);
+            this.pictureBox2.TabIndex = 26;
+            this.pictureBox2.TabStop = false;
+            // 
             // Lot_C_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(750, 652);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Butt_CHK_A);
             this.Controls.Add(this.CHK_C10);
@@ -190,8 +254,12 @@ namespace SWE_Final_Project
             this.Controls.Add(this.CHK_C3);
             this.Controls.Add(this.CHK_C2);
             this.Controls.Add(this.CHK_C1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Lot_C_Form";
             this.Text = "Lot_C_Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,5 +280,7 @@ namespace SWE_Final_Project
         
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button Butt_CHK_A;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
